97a55156fd7ededf6f34f13da58e4081bc8a21ce
“Anupam98837” <anupamchakraborty@hallienz.com>
2026-02-07 13:02:20 +0530

    favicon change

--- resources/views/modules/course/viewCourse/viewCourseLayout.blade.php
+++ resources/views/modules/course/viewCourse/viewCourseLayout.blade.php
@@ -34,6 +34,7 @@
       }catch(e){}
     })();
   </script>
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/media/images/web/favicon.png') }}">
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
